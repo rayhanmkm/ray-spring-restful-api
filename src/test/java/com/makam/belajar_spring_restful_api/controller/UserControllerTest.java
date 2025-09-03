4bdccefbc635fcd9fdb994eb276e086f9b3403be
rayhan atallah <rayhanmkm09@gmail.com>
2025-09-03 20:06:21 +0700

    menambahkan unittest register badrequest dan unittest register duplicate

--- src/test/java/com/makam/belajar_spring_restful_api/controller/UserControllerTest.java
+++ src/test/java/com/makam/belajar_spring_restful_api/controller/UserControllerTest.java
@@ -2,9 +2,11 @@ package com.makam.belajar_spring_restful_api.controller;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.makam.belajar_spring_restful_api.entity.User;
 import com.makam.belajar_spring_restful_api.model.RegisterUserRequest;
 import com.makam.belajar_spring_restful_api.model.WebResponse;
 import com.makam.belajar_spring_restful_api.repository.UserRepository;
+import com.makam.belajar_spring_restful_api.security.BCrypt;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -56,6 +58,59 @@ class UserControllerTest {
                     new TypeReference<>() {}
             );
             assertEquals("OK", response.getData());
+        });
+    }
+
+    @Test
+    void testRegisterBadRequest() throws Exception {
+        RegisterUserRequest request = new RegisterUserRequest();
+        request.setUsername("");
+        request.setPassword("");
+        request.setName("");
+
+        mockMvc.perform(
+                post("/api/users")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+        ).andExpectAll(
+                status().isBadRequest()
+        ).andDo(result -> {
+            WebResponse<String> response = objectMapper.readValue(
+                    result.getResponse().getContentAsString(),
+                    new TypeReference<>() {}
+            );
+            assertNotNull(response.getErrors());
+        });
+    }
+
+    @Test
+    void testRegisterDuplicate() throws Exception {
+        User user = new User();
+        user.setUsername("test");
+        user.setPassword(BCrypt.hashpw("test", BCrypt.gensalt()));
+        user.setName("Test");
+        userRepository.save(user);
+
+
+        RegisterUserRequest request = new RegisterUserRequest();
+        request.setUsername("test");
+        request.setPassword("rahasia");
+        request.setName("Test");
+
+        mockMvc.perform(
+                post("/api/users")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+        ).andExpectAll(
+                status().isBadRequest()
+        ).andDo(result -> {
+            WebResponse<String> response = objectMapper.readValue(
+                    result.getResponse().getContentAsString(),
+                    new TypeReference<>() {}
+            );
+            assertNotNull(response.getErrors());
         });
     }
 }
